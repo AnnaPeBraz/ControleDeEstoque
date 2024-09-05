a572c8dcbc297a803ecbd6ceed12507eeb94e2d7
unknown <annap.0103@gmail.com>
2024-09-04 22:27:14 -0300

    funcao onsubmitsignupuser esta configurada e funcionando

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,32 +1,48 @@
+import { Response } from 'express';
+import { UserService } from './../../services/user/user.service';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   loginCard = true;
 
   loginForm = this.formBuilder.group({
     email: ['', Validators.required],
-    password: ['', Validators.required]
+    password: ['', Validators.required],
   });
 
   signupForm = this.formBuilder.group({
     name: ['', Validators.required],
     email: ['', Validators.required],
-    password: ['', Validators.required]
+    password: ['', Validators.required],
   });
-  constructor(private formBuilder: FormBuilder) { }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private UserService: UserService
+  ) {}
 
   onSubitLoginForm(): void {
     console.log(this.loginForm.value);
   }
 
   onSubmitSignupForm(): void {
-    console.log(this.signupForm.value);
+    if (this.signupForm.value && this.signupForm.valid) {
+      this.UserService.signupUser(this.signupForm.value as SignupUserRequest)
+      .subscribe({
+        next: (Response) => {
+          if(Response){
+            alert('usuario teste deu certo');
+          }
+        },
+        error: (err) => console.log(err),
+      });
+    }
   }
-
 }
