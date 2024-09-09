bb68c091a7ba68cd98e1bc6fc3b3c3b152c4b4ea
unknown <annap.0103@gmail.com>
2024-09-09 14:49:42 -0300

    criar e autenticar usuario

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,8 +1,10 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Response } from 'express';
-import { UserService } from './../../services/user/user.service';
-import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { UserService } from './../../services/user/user.service';
+import { Component } from '@angular/core';
+import { AuthRequest } from 'src/app/services/user/auth/AuthRequest';
 
 @Component({
   selector: 'app-home',
@@ -25,11 +27,21 @@ export class HomeComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-    private UserService: UserService
+    private UserService: UserService,
+    private cookieService: CookieService
   ) {}
 
   onSubitLoginForm(): void {
-    console.log(this.loginForm.value);
+    if (this.loginForm.value && this.loginForm.valid){
+      this.UserService.authUser(this.loginForm.value as AuthRequest).subscribe({
+        next: (response) => {
+          if (response) {
+            this.cookieService.set('USER_INFO', response?.token);
+            this.loginForm.reset();
+          }
+        }
+      })
+    }
   }
 
   onSubmitSignupForm(): void {
@@ -39,6 +51,8 @@ export class HomeComponent {
         next: (Response) => {
           if(Response){
             alert('usuario teste deu certo');
+            this.signupForm.reset();
+            this.loginCard = true;
           }
         },
         error: (err) => console.log(err),
