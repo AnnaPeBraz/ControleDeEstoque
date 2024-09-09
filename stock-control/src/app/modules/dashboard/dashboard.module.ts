c4cd655d61a6d0dfdd413e6f7deded2b0003d4a4
unknown <annap.0103@gmail.com>
2024-09-09 16:41:42 -0300

    add providers e modules no dashboard module

--- stock-control/src/app/modules/dashboard/dashboard.module.ts
+++ stock-control/src/app/modules/dashboard/dashboard.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DasboardHomeComponent } from './page/dasboard-home/dasboard-home.component';
-
+import { RouterModule } from '@angular/router';
+import { DASHBOARD_ROUTES } from './dashboard.routing';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule, SideBarModule } from 'primeng/sidebar';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule, ToolBarModule } from 'primeng/toolbar';
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 
 
 @NgModule({
-  declarations: [
-    DasboardHomeComponent
-  ],
+  declarations: [DasboardHomeComponent],
   imports: [
-    CommonModule, FormsModule, ReactiveFormsModule
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(DASHBOARD_ROUTES),
+    // primeng
+    SidebarModule,
+    ButtonModule,
+    ToolbarModule,
+    CardModule,
+    ToastModule,
+  ],
+  providers: [CookieService, MessageService]
 })
-export class DashboardModule { }
+
+export class DashboardModule {}
