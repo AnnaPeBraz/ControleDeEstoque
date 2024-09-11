b65452596c139e6ddd5183ff6b8cd569902e9299
unknown <annap.0103@gmail.com>
2024-09-10 22:17:26 -0300

    implementando guarda de rotas, mas nao ta funfando

--- stock-control/src/app/app-routing.module.ts
+++ stock-control/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
-import { DasboardHomeComponent } from './modules/dashboard/page/dasboard-home/dasboard-home.component';
+import { AuthGuard } from './guards/auth-guard.service';
 
 const routes: Routes = [
   {
@@ -17,7 +17,10 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: () =>
       import(
-        './modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
+        './modules/dashboard/dashboard.module').then(
+          (m) => m.DashboardModule
+        ),
+        canActivate: [AuthGuard]  
   },
 ];
 
--- stock-control/src/app/services/user/user.service.ts
+++ stock-control/src/app/services/user/user.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
-import { environment } from 'src/environments/environment';
 import { AuthRequest } from './auth/AuthRequest';
 import { AuthResponse } from './auth/AuthResponse';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +14,22 @@ import { AuthResponse } from './auth/AuthResponse';
 export class UserService {
   private API_URL = environment.API_URL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookie: CookieService) { }
 
   signupUser(requestDatas: SignupUserRequest): Observable<SignupUserResponse> {
     return this.http.post<SignupUserResponse>(
       `${this.API_URL}/user`,
       requestDatas
-    )
+    );
   }
 
   authUser(requestDatas: AuthRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas);
+  }
+
+  isLoggedIn(): boolean{
+    //verificar se o user possui um token ou um cookie
+    const JWT_TOKEN = this.cookie.get('USER_INFO');
+    return JWT_TOKEN ? true : false;
   }
 }
